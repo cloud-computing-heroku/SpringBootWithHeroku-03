b7d0754e6b58b0218f9bfeaca2cfce0e281872b2
HajPhan <hajphan97@gmail.com>
2019-08-17 21:40:28 +0700

    First commit :smile:

---
+++ src/main/java/com/example/SpringBootWithHeroku03/SpringBootWithHeroku03Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithHeroku03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithHeroku03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithHeroku03Application.class, args);
+	}
+
+}
